54fa86b897fe4c23f4e20903db6fd215bbb875c9
Christian Arellano <78322432+chrisare1718@users.noreply.github.com>
2023-03-27 22:21:50 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Hashtable hashtable = new Hashtable();
+
+        // add thousands of string values to the hashtable
+        for (int i = 10000000; i > 1; i--)
+        {
+            string key = "key" + i.ToString();
+            string v = "value" + i.ToString();
+            hashtable.Add(key, v);
+        }
+
+        // iterate over all key-value pairs in the hashtable
+        foreach (DictionaryEntry entry in hashtable)
+        {
+            Console.WriteLine(entry.Key + ": " + entry.Value);
+        }
+        
+        string[] A = new string[10000000];
+        for(int i = 0; i < 10000000; i++)
+        {
+            A[i] = i.ToString();
+        }
+        foreach(string s in A)
+        {
+            Console.WriteLine(s);
+        }
+        
+        Console.ReadLine();
+    }
+}
